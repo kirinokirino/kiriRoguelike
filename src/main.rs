60adf4f3b0a7b6c35285a465971c2a78447f0dde
kirinokirino <kirinokirin2501@gmail.com>
2024-01-06 00:48:35 +0300

    Remove specific-outdated clippy configuration.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,3 @@
-#![deny(clippy::all)]
-#![warn(
-    clippy::pedantic,
-    clippy::cargo,
-    clippy::nursery,
-    clippy::clone_on_ref_ptr,
-    clippy::else_if_without_else,
-    clippy::float_cmp_const,
-    clippy::let_underscore_must_use,
-    clippy::mem_forget,
-    clippy::multiple_inherent_impl,
-    clippy::rest_pat_in_fully_bound_structs,
-    clippy::unwrap_used,
-    clippy::shadow_reuse,
-    clippy::shadow_same,
-    clippy::string_add,
-    clippy::wildcard_enum_match_arm,
-    clippy::wrong_pub_self_convention
-)]
-#![allow(
-    clippy::missing_docs_in_private_items,
-    clippy::module_name_repetitions,
-    clippy::future_not_send
-)]
-
 use macroquad::{
     clear_background, debug, draw_circle, draw_text, is_key_pressed, is_mouse_button_down,
     next_frame, set_camera, set_default_camera, warn, Camera2D, Color, KeyCode, MouseButton, Vec2,
